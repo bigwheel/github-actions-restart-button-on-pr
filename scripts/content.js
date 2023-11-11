b3198293ffef1278a1f858dc4ef7e63f2b352bb1
k.bigwheel <k.bigwheel+eng@gmail.com>
2023-11-11 23:53:25 +0900

    Implementing

--- scripts/content.js
+++ scripts/content.js
@@ -1,14 +1,36 @@
 // TODO: https://stackoverflow.com/questions/19191679/chrome-extension-inject-js-before-page-load
-console.log("load content script");
+
+function createRestartButton() {
+  const button = document.createElement("span");
+  button.textContent = "Restart";
+  button.className = "btn btn-sm";
+
+  return button;
+}
+
+function alreadyInserted(parentNode, insertingNode) {
+  parentNode.childNodes.forEach((node) => {
+    if (node.isEqualNode(insertingNode)) return true;
+  });
+  return false;
+}
+
+i = 0;
+
 const observer = new MutationObserver(() => {
-  const ghaDetailsLink = "div.merge-status-list > div > div > a";
-  const links = document.querySelectorAll(ghaDetailsLink);
-  links.forEach((link) => {
+  const detailsLinkSelector = "div.merge-status-list > div > div > a";
+  const detailsLinks = document.querySelectorAll(detailsLinkSelector);
+  detailsLinks.forEach((link) => {
     console.log(link.parentNode.childNodes);
-    const button = document.createElement("span");
-    button.textContent = "Restart";
-    link.parentNode.insertBefore(button, link);
-    observer.disconnect();
+    const button = createRestartButton();
+    if (!alreadyInserted(link.parentNode, button)) {
+      link.parentNode.insertBefore(button, link);
+      i = i + 1;
+      if (100 < i) {
+        console.log("abort");
+        observer.disconnect();
+      }
+    }
   });
 
   console.log("Added div element");
