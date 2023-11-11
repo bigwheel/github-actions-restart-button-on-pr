dbc2b86394e422303e97a3a89b4bd18233b38247
k.bigwheel <k.bigwheel+eng@gmail.com>
2023-11-12 01:54:44 +0900

    Success Restart with github token

--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,18 @@
 // TODO: https://stackoverflow.com/questions/19191679/chrome-extension-inject-js-before-page-load
 
-function pushRestartButton() {
-  console.log("pushed");
+function createPushRestartButton(targetUrl) {
+  return () => {
+    console.log(targetUrl);
+
+  fetch(targetUrl, {
+    method: "POST",
+    headers: {
+      "Accept": "application/vnd.github+json",
+      "Authorization": "Bearer aaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+      "X-GitHub-Api-Version": "2022-11-28"
+    }
+  });
+  };
 }
 
 function createRestartButton(workflowRunUrl) {
@@ -10,10 +21,18 @@ function createRestartButton(workflowRunUrl) {
   button.className = "btn btn-sm";
 
   // https://github.com/org_name/repo_name/actions/runs/1824374999/job/58560149807?pr=4012
-  const urlRegex = new RegExp("^https://github\\.com/(?<org>[^/]+)/(?<repo>[^/]+)/actions/runs/(?<workflow_id>\\d+)/job/(?<job_id>\\d+).*$");
-  console.log(workflowRunUrl.match(urlRegex));
+  const urlRegex = new RegExp(
+    "^https://github\\.com/(?<org>[^/]+)/(?<repo>[^/]+)/actions/runs/(?<run_id>\\d+)/job/(?<job_id>\\d+).*$"
+  );
+  const matchResult = workflowRunUrl.match(urlRegex);
+  const org = matchResult.groups["org"];
+  const repo = matchResult.groups["repo"];
+  const run_id = matchResult.groups["run_id"];
+  console.log(matchResult);
+  const targetUrl = `https://api.github.com/repos/${org}/${repo}/actions/runs/${run_id}/rerun`;
+  console.log(targetUrl);
 
-  button.addEventListener("click", pushRestartButton);
+  button.addEventListener("click", createPushRestartButton(targetUrl));
 
   return button;
 }
